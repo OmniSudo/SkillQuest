e4d3e1021d89edeaf50a6015eb5c29184905dce5
omnisudo <omnisudo@gmail.com>
2024-11-11 07:29:27 -0600

    Implement asset locations

--- SkillQuest.Shared.Game/src/Assets/Location.cs
+++ SkillQuest.Shared.Game/src/Assets/Location.cs
@@ -4,8 +4,20 @@ namespace SkillQuest.Shared.Game.Assets;
 
 public class Location{
     public Uri Uri { get; }
-    
-    public Location( IAddon addon, string path){
-        Uri = new Uri( addon.Uri!, path );
+
+    public Location(IAddon addon, string path){
+        string side = "Shared";
+
+        switch (addon.Uri.Scheme) {
+            case "cl":
+            case "client":
+                side = "Client";
+                break;
+            case "sv":
+            case "server":
+                side = "Server";
+                break;
+        }
+        Uri = new Uri( "file://" + Path.Combine($"Addons/{addon.Name}/{side}/assets/", path ) );
     }
 }
