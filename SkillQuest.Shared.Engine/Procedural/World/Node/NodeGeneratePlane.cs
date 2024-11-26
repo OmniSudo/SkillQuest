76770946070aa8f564f8f0490ca8add76f240b26
omnisudo <omnisudo@gmail.com>
2024-11-26 11:08:21 -0600

    Clear out generate plane node

--- SkillQuest.Shared.Engine/Procedural/World/Node/NodeGeneratePlane.cs
+++ SkillQuest.Shared.Engine/Procedural/World/Node/NodeGeneratePlane.cs
@@ -6,10 +6,6 @@ public class NodeGeneratePlane{
     public double Y { get; set; } = 0;
 
     public void Generate(IRegion region){
-        foreach (var x in Enumerable.Range(0, 64)) {
-            foreach (var z in Enumerable.Range(0, 64)) {
-                
-            }
-        }
+        
     }
 }
