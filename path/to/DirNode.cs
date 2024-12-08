d89a155a2f4c4b51596c33b8e9818a78439981dc
omnisudo <omnisudo@gmail.com>
2024-12-07 18:48:38 -0600

    Move private to make it public. Server can be open source!

--- path/to/DirNode.cs
+++
@@ -1,9 +0,0 @@
-// entire file content ...
-public class DirNode {
-    public string Name { get; set; }
-    public DirNode Parent { get; set; }
-    public Dictionary<string, DirNode> Children { get; set; }
-    public IThing Thing { get; set; }
-
-    public bool Opened { get; set; } = false;
-}
